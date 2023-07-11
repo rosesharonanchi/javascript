0b14d65b8b1aed3d788c083e114ea0ab7ba80142
rosesharonanchi <rosesharonanchi123@gmail.com>
2023-07-11 06:31:01 -0700

    initial commit

---
+++ introduction.js
@@ -0,0 +1,15 @@
+// This is a comment
+/*
+This is a multyline commment
+ */
+// variable declaration
+var myVar="hello world";
+let myVariable="10"
+const variable=12
+console.log(myVar)
+console.log(myVariable)
+console.log(typeof(myVar))
+var a=2
+   b=10
+console.log(a+b)
+//increment and decrement
